d50c906f12ff6e58020689d7c47900e12c565330
Tarun_Raghav3699 <124661867+TarunRaghav3699@users.noreply.github.com>
2023-10-27 09:31:50 +0000

    .

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
+'''simple website to check AQI'''
 from flask import Flask
 import requests
 
-from flask import Flask
+# from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    """API calling here"""
     response = requests.get("https://api.airvisual.com/v2/nearest_city?key=1589f34c-b54c-4647-bfe3-ecef91f56e17")
     return response.json()
 
 if __name__ == "__main__":
     app.run(debug=True) 
+
+
+    """https://dev.to/bluepaperbirds/get-all-keys-and-values-from-json-object-in-python-1b2d visit this website"""
